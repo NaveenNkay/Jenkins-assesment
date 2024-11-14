4b4c49ac2229f44f5d685bee53caab9063d50270
NaveenNkay <147308581+NaveenNkay@users.noreply.github.com>
2024-11-14 22:53:30 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "google" {
-  project = "quick-assesment" 
-  region  = "asia-east1"      
-}
-resource "google_compute_network" "assessment-vpc" {
-  name                    = "assessment-vpc"
-  auto_create_subnetworks  = false  # Setting this to false means we will create custom subnets
-  routing_mode             = "GLOBAL"  # Regional routing mode for better control
-}
