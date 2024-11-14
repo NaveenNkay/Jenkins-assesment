07053bd37971140cf3d7a3451e138b7c5bdb1348
NaveenNkay <147308581+NaveenNkay@users.noreply.github.com>
2024-11-14 18:51:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "google_compute_network" "assessment-vpc" {
+  name                    = "assessment-vpc"
+  auto_create_subnetworks  = false  # Setting this to false means we will create custom subnets
+  routing_mode             = "GLOBAL"  # Regional routing mode for better control
+}
