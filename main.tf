965cc3d0fbb5e5c1c5ab408cd1e2b71d92eec212
NaveenNkay <147308581+NaveenNkay@users.noreply.github.com>
2024-11-14 18:52:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "google" {
+  project = "quick-assesment" 
+  region  = "us-central1"      
+}
 resource "google_compute_network" "assessment-vpc" {
   name                    = "assessment-vpc"
   auto_create_subnetworks  = false  # Setting this to false means we will create custom subnets
